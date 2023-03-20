657c5195894db4b90f3ed4d4e537f13bc50744f9
Kaouthar <btssiokaou@gmail.com>
2023-03-20 14:40:03 +0100

    création de l'interface istasionable

--- Interfaces/Istationable.cs
+++ Interfaces/Istationable.cs
@@ -11,6 +11,13 @@ namespace Station.Interfaces
         void EnregistrerArriveePrevue(object objet);
         void EnregistrerArrivee(String id);
         void EnregistrerDepart(String id);
+
+        bool EstAttendu(String id);
+        bool EstPresent(String id);
+        bool EstParti(String id);
+        object GetUnAttendu(String id);
+        object GetUnArrive(String id);
+        object GetUnParti(String id);
     
     }
 }
