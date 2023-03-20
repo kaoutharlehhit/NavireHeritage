804d1417c0c64dff989655761ac0f01a109c60c1
Kaouthar <btssiokaou@gmail.com>
2023-03-20 16:17:58 +0100

    une partie de la class croisière

--- ClassesMetier/Croisière.cs
+++ ClassesMetier/Croisière.cs
@@ -1,27 +1,60 @@
-﻿using System;
+﻿using GestionNavire.Exceptions;
+using Station.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NavireHeritage.classesMetier
 {
-    class Croisière : Navire
+    class Croisière : Navire, ICroisierable
     {
         private String typeNavireCroisière;
         private int nbPassagersMaxi;
-        private Dictionary<String, Passager> Passagers;
+        private Dictionary<String, Passager> passagers;
 
-        public Croisière(string imo, string nom, string latitude, string longitude, double tonnageActuel, double tonnageGT, double tonnageDWT,string typeNavireCroisière, int nbPassagersMaxi, Dictionary<string, Passager> passagers) :base(imo, nom, latitude, longitude, tonnageActuel, tonnageGT, tonnageDWT)
+
+        public Croisière(string imo, string nom, string latitude, string longitude, double tonnageActuel, double tonnageGT, double tonnageDWT, string typeNavireCroisière, int nbPassagersMaxi)
+            : base(imo, nom, latitude, longitude, tonnageActuel, tonnageGT, tonnageDWT)
         {
             this.typeNavireCroisière = typeNavireCroisière;
             this.nbPassagersMaxi = nbPassagersMaxi;
-            Passagers = passagers;
+        }
+        public Croisière(string imo, string nom, string latitude, string longitude, double tonnageActuel, double tonnageGT, double tonnageDWT,string typeNavireCroisière, int nbPassagersMaxi, Dictionary<string, Passager> passagers) 
+            :base(imo, nom, latitude, longitude, tonnageActuel, tonnageGT, tonnageDWT)
+        {
+            this.typeNavireCroisière = typeNavireCroisière;
+            this.nbPassagersMaxi = nbPassagersMaxi;
+            this.Passagers = passagers;
             
         }
 
         public string TypeNavireCroisière { get => typeNavireCroisière; set => typeNavireCroisière = value; }
         public int NbPassagersMaxi { get => nbPassagersMaxi; set => nbPassagersMaxi = value; }
-        internal Dictionary<string, Passager> Passagers1 { get => Passagers; set => Passagers = value; }
+        internal Dictionary<string, Passager> Passagers { get => this.passagers; set => this.passagers = value; }
+
+
+        void Embarquer(List<Object> objets)
+        {
+            foreach(Passager passager in objets) {
+                if(this.Passagers.Count < this.nbPassagersMaxi)
+                {
+                    this.Passagers.Add(passager.NumPasseport, passager);
+                }
+                else
+                {
+                    throw new GestionPortExceptions("Le bâteau ne peut acceuillir plus de passager");
+                }
+            }
+        }
+        void Debarquer(List<Object> objects)
+        {
+            foreach(Passager passager in objects)
+            {
+
+            }
+        }
     }
 }
